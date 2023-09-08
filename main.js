c5107cd33e07ca185bc5c9eb25067ddf5ca841fa
Babatunde Owolabi <chatwittundeowolabi@gmail.com>
2023-09-08 19:44:21 +0100

    correction

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const now = new Date();
 const day = now.getDay(); 
-const hours = now.getHours();
-const minutes = now.getMinutes();
+const utcInMilliSeconds = now.getUTCMilliseconds();
 
-const utcHours = hours + 1;
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const dayEl = document.querySelector('#day');
 const timeEl = document.querySelector('#time');
 
-const currentTime = minutes < 10 ? `${utcHours}:0${minutes}` : `${utcHours}:${minutes}`
+const currentTime = utcInMilliSeconds;
 
 dayEl.innerHTML = weekDays[day];
 timeEl.innerHTML = currentTime;
+
